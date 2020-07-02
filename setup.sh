ca18396a14b08abeaba5b1e9d90ce1d2373d433b
Johannes Konze <johannes.konze@gute-weine.com>
2020-07-02 09:54:35 +0200

    remove empty install line

--- setup.sh
+++ setup.sh
@@ -38,7 +38,6 @@ brew cask install spotify
 brew cask install insomnia
 brew cask install typora
 brew cask install figma
-brew cask install
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
